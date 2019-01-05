ccd69c444ecbd413d9cede8158ce3ce58280d0c3
vincenzo <speedvicio@hotmail.com>
2019-01-05 13:58:44 +0100

    - Change a bit the Rebuild window layout

--- Laziass/RebuildImage.Designer.vb
+++ Laziass/RebuildImage.Designer.vb
@@ -26,34 +26,47 @@ Partial Class RebuildImage
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button2
         '
-        Me.Button2.Image = Global.LazyAss.My.Resources.Resources.clonecd
-        Me.Button2.Location = New System.Drawing.Point(12, 12)
+        Me.Button2.BackgroundImage = Global.LazyAss.My.Resources.Resources.clonecd
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button2.Location = New System.Drawing.Point(37, 35)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 73)
+        Me.Button2.Size = New System.Drawing.Size(50, 50)
         Me.Button2.TabIndex = 0
-        Me.ToolTip1.SetToolTip(Me.Button2, "Rebuild in ccd/img/sub format")
+        Me.ToolTip1.SetToolTip(Me.Button2, "Rebuild in ccd/img/sub format" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(Raccomended option)")
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button1
         '
+        Me.Button1.BackgroundImage = Global.LazyAss.My.Resources.Resources.cdrwin
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
         Me.Button1.Enabled = False
-        Me.Button1.Image = Global.LazyAss.My.Resources.Resources.cdrwin
-        Me.Button1.Location = New System.Drawing.Point(94, 12)
+        Me.Button1.Location = New System.Drawing.Point(93, 35)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 73)
+        Me.Button1.Size = New System.Drawing.Size(50, 50)
         Me.Button1.TabIndex = 1
-        Me.ToolTip1.SetToolTip(Me.Button1, "Rebuild in cue/bin format")
+        Me.ToolTip1.SetToolTip(Me.Button1, "Rebuild in cue/bin format" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(Reccomended only for ""Redump"" file format)")
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(147, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Select Destination File Format"
         '
         'RebuildImage
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(181, 97)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
@@ -61,10 +74,12 @@ Partial Class RebuildImage
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Rebuild Splitted Image"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents Label1 As Label
 End Class
