89186b0f8a092325938ab65e86b64e0780f346ef
Speedvicio <speedvicio@hotmail.com>
2020-12-16 17:08:49 +0100

    - Added more specific instruction to rebuild Cfs/Ciso to Zip

--- Laziass/FPismo.Designer.vb
+++ Laziass/FPismo.Designer.vb
@@ -22,46 +22,62 @@ Partial Class FPismo
     'Non modificarla mediante l'editor del codice.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
         '
         Me.Button1.BackgroundImage = Global.LazyAss.My.Resources.Resources.convert
         Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-        Me.Button1.Location = New System.Drawing.Point(37, 36)
+        Me.Button1.Location = New System.Drawing.Point(56, 39)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(50, 50)
         Me.Button1.TabIndex = 0
+        Me.ToolTip1.SetToolTip(Me.Button1, "Convert to Ciso or rebuilt to Zip")
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
         Me.Button2.BackgroundImage = Global.LazyAss.My.Resources.Resources.create
         Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-        Me.Button2.Location = New System.Drawing.Point(93, 36)
+        Me.Button2.Location = New System.Drawing.Point(112, 39)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(50, 50)
         Me.Button2.TabIndex = 1
+        Me.ToolTip1.SetToolTip(Me.Button2, "Convert to Cfs")
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(9, 7)
+        Me.Label1.Location = New System.Drawing.Point(28, 7)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(163, 26)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "Convert a folder of cd image into " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "ciso or cfs compressed format"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 96)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(194, 52)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "**Specify the output file extension:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "- Ciso for convert from cd image" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "- Cfs for" &
+    " create from folder to cfs" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "- Zip for conversion from Ciso/Cfs to Zip"
+        '
         'FPismo
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(181, 97)
+        Me.ClientSize = New System.Drawing.Size(218, 158)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
@@ -77,4 +93,6 @@ Partial Class FPismo
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Label1 As Label
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents Label2 As Label
 End Class
